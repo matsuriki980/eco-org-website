21461e993929872528f38a2490425db9c9338909
matsuyamarikiya <maturiki17@ymail.ne.jp>
2025-09-12 13:31:45 +0900

    2025年 9月 12日 作業日報

--- js/script.js
+++ js/script.js
@@ -8,3 +8,28 @@ hamburgerButton.addEventListener("click", () => {
   hamburgerButton.classList.toggle("active");
   hamburgerMenu.classList.toggle("active");
 });
+
+
+// ==========================================================================
+// ヘッダー背景表示・非表示
+// ==========================================================================
+window.addEventListener("scroll", () => {
+  const header = document.getElementById("header");
+  const fv = document.getElementById("fv");
+
+  const getscrollPercentFv = () => {
+    // ファーストビューの高さ
+    const fvHeight = fv.offsetHeight;
+
+    // スクロール量
+    const scrolled = window.scrollY;
+
+    if (scrolled > fvHeight) {
+      header.classList.add("active");
+    } else {
+      header.classList.remove("active");
+    }
+  };
+
+  getscrollPercentFv();
+});
